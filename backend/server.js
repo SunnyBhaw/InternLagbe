18ee7e34d32b049006d22262ee8f12bc37ab0b93
SunnyBhaw <mirzahidulalam2000@gmail.com>
2026-02-05 17:28:01 +0600

    Backend ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "*",
+    origin: "https://internlagbe.onrender.com",
     credentials: true
 }));
 app.use(express.json());
