3dcae656ae0fcfcd3ef2c61b5f3a22823f9d4eb2
SunnyBhaw <mirzahidulalam2000@gmail.com>
2026-02-05 14:39:09 +0600

    Backend ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    credentials: true
+}));
 app.use(express.json());
 
 // Set static folder for uploads
